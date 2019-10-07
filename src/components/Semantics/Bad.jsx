3d1d3fab8649303cfe6067145cef4dd9ef008072
Aaron Osteraas <aaron.osteraas@gmail.com>
2019-10-07 21:30:11 +1100

    semantic html

---
+++ src/components/Semantics/Bad.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const Bad = () => {
+  return (
+    <div className="main">
+      <div className="header">Cool Page</div>
+      <div className="main">
+        <div className="title">Cool Content</div>
+        <div className="content">
+          <p>How's about that sports game</p>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ src/components/Semantics/Good.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const Good = () => {
+  return (
+    <main>
+      <header>Cool Page</header>
+      <article>
+        <h1>Cool Content</h1>
+        <section>
+          <p>How's about that sports game</p>
+        </section>
+      </article>
+    </main>
+  );
+};
